592453516e38696e1c0770517f894171d9049721
Tung Linh Le <linh1987@gmail.com>
2021-11-17 00:28:04 +0100

    Add error logging

--- api/crawlers/alphaspel.js
+++ api/crawlers/alphaspel.js
@@ -1,73 +1,78 @@
-const cheerio = require('cheerio');
-const request = require('request');
+const cheerio = require("cheerio");
+const request = require("request");
 
-const searchUrlTemplate = 'https://www.alphaspel.se/search/?query=';
-const domainPrefix = 'https://www.alphaspel.se';
-
+const searchUrlTemplate = "https://www.alphaspel.se/search/?query=";
+const domainPrefix = "https://www.alphaspel.se";
 
 const parseHtmlToProducts = (response) => {
-    const $ = cheerio.load(response);
-    const foundProducts = $('.product').map((_, el) => {
-        const $el = $(el);
-        const game = {
-            url: domainPrefix + $el.find('a').first().attr('href'),
-            name: $el.find('.product-name').text().trim(),
-            image: domainPrefix + $el.find('img').first().attr('src'),
-            price: $el.find('.price').first().text().trim(),
-            available: $el.find('.add-to-cart').text().indexOf('Köp') > -1
+  const $ = cheerio.load(response);
+  const foundProducts = $(".product")
+    .map((_, el) => {
+      const $el = $(el);
+      const game = {
+        url: domainPrefix + $el.find("a").first().attr("href"),
+        name: $el.find(".product-name").text().trim(),
+        image: domainPrefix + $el.find("img").first().attr("src"),
+        price: $el.find(".price").first().text().trim(),
+        available: $el.find(".add-to-cart").text().indexOf("Köp") > -1,
+      };
+
+      return game;
+    })
+    .get();
+
+  return foundProducts;
+};
+
+const fetchByUrl = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      }
+
+      let httpError = "";
+
+      if (response) {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          resolve({
+            name: "alphaspel",
+            url: "",
+            games: parseHtmlToProducts(body),
+          });
+          return;
         }
 
-        return game;
-    }).get();
+        httpError = response.statusCode;
+        console.log("alphaspel Unknown error: " + response.statusCode);
+      }
 
-    return foundProducts;
-}
-
-const fetchByUrl = url => {
-    return new Promise((resolve, reject) => {
-        request(url, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            }
-
-            if (response) {
-                if (response.statusCode >= 200 && response.statusCode < 300) {
-                    resolve({
-                        name: 'alphaspel',
-                        url: '',
-                        games: parseHtmlToProducts(body)
-                    });
-                    return;
-                }
-                
-                console.log('alphaspel Unknown error: ' + response.statusCode);
-            }
-
-            resolve({
-                name: 'alphaspel',
-                url: '',
-                games: []
-            });
-        });
+      resolve({
+        name: "alphaspel",
+        url: "",
+        error: error && error.toString ? error.toString() : "",
+        httpError,
+        games: [],
+      });
     });
-}
-
+  });
+};
 
 const search = (term) => {
-    const concenatedTerm = term.replace(' ', '+');
+  const concenatedTerm = term.replace(" ", "+");
 
-    const searchUrl = searchUrlTemplate + concenatedTerm;
+  const searchUrl = searchUrlTemplate + concenatedTerm;
 
-    return fetchByUrl(searchUrl);
-}
+  return fetchByUrl(searchUrl);
+};
 
-const newArrivalsUrlTemplate = 'https://alphaspel.se/491-bradspel/news/';
+const newArrivalsUrlTemplate = "https://alphaspel.se/491-bradspel/news/";
 
 const fetchNewArrivals = () => {
-    return fetchByUrl(newArrivalsUrlTemplate);
-}
+  return fetchByUrl(newArrivalsUrlTemplate);
+};
 
 module.exports = {
-    search,
-    fetchNewArrivals
-}
+  search,
+  fetchNewArrivals,
+};
--- api/crawlers/dragonlair.js
+++ api/crawlers/dragonlair.js
@@ -1,71 +1,78 @@
-const cheerio = require('cheerio');
-const request = require('request');
+const cheerio = require("cheerio");
+const request = require("request");
 
-const searchUrlTemplate = 'https://www.dragonslair.se/product/?name=';
-const domainPrefix = 'https://www.dragonslair.se';
-
+const searchUrlTemplate = "https://www.dragonslair.se/product/?name=";
+const domainPrefix = "https://www.dragonslair.se";
 
 const parseHtmlToProducts = (response) => {
-    const $ = cheerio.load(response);
-    const foundProducts = $('.grid .item').map((_, el) => {
-        const $el = $(el);
-        const game = {
-            url: domainPrefix + $el.find('a').first().attr('href'),
-            name: $el.find('.title').text().trim(),
-            image: domainPrefix + $el.find('.image img').first().attr('src'),
-            price: $el.find('.price').first().text().trim(),
-            available: $el.find('.stock .description').text().indexOf('Ja') > -1
+  const $ = cheerio.load(response);
+  const foundProducts = $(".grid .item")
+    .map((_, el) => {
+      const $el = $(el);
+      const game = {
+        url: domainPrefix + $el.find("a").first().attr("href"),
+        name: $el.find(".title").text().trim(),
+        image: domainPrefix + $el.find(".image img").first().attr("src"),
+        price: $el.find(".price").first().text().trim(),
+        available: $el.find(".stock .description").text().indexOf("Ja") > -1,
+      };
+
+      return game;
+    })
+    .get();
+
+  return foundProducts;
+};
+
+const fetchByUrl = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      }
+      let httpError = "";
+
+      if (response) {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          resolve({
+            name: "Dragon's Lair",
+            url: url,
+            games: parseHtmlToProducts(body),
+          });
+          return;
         }
 
-        return game;
-    }).get();
+        httpError = response.statusCode;
+        console.log("Dragon's Lair Unknown error: " + response.statusCode);
+      }
 
-    return foundProducts;
-}
-
-const fetchByUrl = url => {
-    return new Promise((resolve, reject) => {
-        request(url, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            }
-            if (response) {
-                if (response.statusCode >= 200 && response.statusCode < 300) {
-                    resolve({
-                        name: 'Dragon\'s Lair',
-                        url: '',
-                        games: parseHtmlToProducts(body)
-                    });
-                    return;
-                }
-                console.log('Dragon\'s Lair Unknown error: ' + response.statusCode);
-            }
-
-            
-            resolve({
-                name: 'Dragon\'s Lair',
-                url: '',
-                games: []
-            });
-        });
+      resolve({
+        name: "Dragon's Lair",
+        url: url,
+        error: error && error.toString ? error.toString() : "",
+        httpError,
+        games: [],
+      });
     });
-}
+  });
+};
 
 const search = (term) => {
-    const concenatedTerm = term.replace(' ', '+');
+  const concenatedTerm = term.replace(" ", "+");
 
-    const searchUrl = `https://www.dragonslair.se/product/name:${concenatedTerm}/boardgame`;
+  const searchUrl = `https://www.dragonslair.se/product/name:${concenatedTerm}/boardgame`;
 
-    return fetchByUrl(searchUrl);
-}
+  return fetchByUrl(searchUrl);
+};
 
-const newArrivalsUrlTemplate = 'https://www.dragonslair.se/product/sort:new-arrivals/boardgame';
+const newArrivalsUrlTemplate =
+  "https://www.dragonslair.se/product/sort:new-arrivals/boardgame";
 
 const fetchNewArrivals = () => {
-    return fetchByUrl(newArrivalsUrlTemplate);
-}
+  return fetchByUrl(newArrivalsUrlTemplate);
+};
 
 module.exports = {
-    search,
-    fetchNewArrivals
-}
+  search,
+  fetchNewArrivals,
+};
